901d68a5ce2c59d9040e566a86393c3a6a4c87c5
Vasil Trendafilov <vasetrendafilov@gmail.com>
2022-07-09 00:53:51 +0200

    docs for template
    - updated readme how to use
    - added docs to the class

--- README.md
+++ README.md
@@ -25,19 +25,22 @@ List the dependencies in the files `dependencies-apt.txt` and
 
 ### 4. Place your code
 
-Place your code in the directory `/packages/duckiebot` of
-your new repository.
-
+Place your code in the directory `packages/duckiebot` of your new repository. The package contains:
+* Main file is placed in `src/duckiebot.py`.
+* Library for communicating with the robot is in `include/my_package/lib_duckiebot.py`.
+* Roslaunch file in `launch/duckiebot.launch` for launching the main file.
 
 ### 5. Setup launchers
 
 The directory `/launchers` can contain as many launchers (launching scripts)
 as you want. A default launcher called `default.sh` must always be present.
 
-If you create an executable script (i.e., a file with a valid shebang statement)
-a launcher will be created for it. For example, the script file 
-`/launchers/my-launcher.sh` will be available inside the Docker image as the binary
-`dt-launcher-my-launcher`.
-
-When launching a new container, you can simply provide `dt-launcher-my-launcher` as
-command.
+### 6. Docker commands
+#### Build on host 
+    dts devel build -f -u duckiebot
+#### Run on host
+    docker run -it --rm  -v ~/Downloads/data:/data --name duckiebot duckiebot/dt-duckiebot-interface:v2-amd64
+#### Build on duckie
+    docker -H patka1.local build -t duckiebot:v1 .
+#### Run on duckie
+    docker -H patka1.local run -it --rm  -v /data:/data --name duckiebot --privileged --network=host duckiebot:v1
--- packages/duckiebot/include/my_package/lib_duckiebot.py
+++ packages/duckiebot/include/my_package/lib_duckiebot.py
@@ -11,57 +11,85 @@ from sensor_msgs.msg import Image, CompressedImage, CameraInfo
 
 class Duckiebot(DTROS):
     def __init__(self,node_name):
+        
         # initialize the DTROS parent class
         super(Duckiebot, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
-
+        
         # Variables
         self.img_count = 0
+        self.name = os.environ['VEHICLE_NAME']
         self.right_encoder_tics = 0
         self.left_encoder_tics = 0
+        self.bridge = CvBridge()
 
-        # Setup Parameters  treba da se namestat
-        self.v_gain = self.setupParam("~speed_gain", 0.41)
-        #self.omega_gain = self.setupParam("~steer_gain", 8.3)
-        #self.bicycle_kinematics = self.setupParam("~bicycle_kinematics", 0)
-        #self.steer_angle_gain = self.setupParam("~steer_angle_gain", 1)
-        #self.simulated_vehicle_length = self.setupParam("~simulated_vehicle_length", 0.18)
+        # Setup Parameters
+        self.rez_h = self.setupParam(f"/{self.name}/camera_node/res_h", 308)
+        self.rez_w = self.setupParam(f"/{self.name}/camera_node/res_w", 408)
+        
+        # To dynamically change the param bash shell into one of the docker containers of the duckie 
+        # and type rosparam set /emergency_stop 2
+        self.emergency_stop = self.setupParam("/emergency_stop",1)
+        # Always get the param before using it
+        self.emergency_stop = rospy.get_param("/emergency_stop")
 
         # Publications
-        self.pub_wheel = rospy.Publisher('/patka1/wheels_driver_node/wheels_cmd',WheelsCmdStamped, queue_size=1)
-        self.pub_car = rospy.Publisher('/patka1/car_cmd_switch_node/cmd',Twist2DStamped, queue_size=1)
+        self.pub_wheel = rospy.Publisher(f'/{self.name}/wheels_driver_node/wheels_cmd',WheelsCmdStamped, queue_size=1)
+        self.pub_car = rospy.Publisher(f'/{self.name}/car_cmd_switch_node/cmd',Twist2DStamped, queue_size=1)
 
         # Subscriptions
-        self.sub_right_encoder = rospy.Subscriber("/patka1/right_wheel_encoder_node/tick", WheelEncoderStamped, self.right_encoder, queue_size=1)
-        self.sub_left_encoder =  rospy.Subscriber("/patka1/left_wheel_encoder_node/tick", WheelEncoderStamped, self.left_encoder, queue_size=1)
+        self.sub_right_encoder = rospy.Subscriber(f"/{self.name}/right_wheel_encoder_node/tick", WheelEncoderStamped, self.right_encoder, queue_size=1)
+        self.sub_left_encoder =  rospy.Subscriber(f"/{self.name}/left_wheel_encoder_node/tick", WheelEncoderStamped, self.left_encoder, queue_size=1)
     
     def right_encoder(self,data):
+        """ Callback function to save right encoder tics. """
         self.right_encoder_tics = data.data
-        rospy.loginfo("I heard that the right wheel is %s tics and resolution of %s", str(data.data),str(data.resolution))
+        #rospy.loginfo("I heard that the right wheel is %s tics and resolution of %s", str(data.data),str(data.resolution))
 
     def left_encoder(self,data):
+        """ Callback function to save left encoder tics. """
         self.left_encoder_tics = data.data
-        rospy.loginfo("I heard that the left wheel is %s tics and resolution of %s", str(data.data),str(data.resolution))
+        #rospy.loginfo("I heard that the left wheel is %s tics and resolution of %s", str(data.data),str(data.resolution))
 
     def connect_camera(self,callback):
-        self.sub_image = rospy.Subscriber("/patka1/camera_node/image/compressed", CompressedImage, callback, queue_size=1)
+        """
+        Initialize and connect to the camera. \n
+        Arguments:
+            callback: Function where you process the image.
+        """
+        self.sub_image = rospy.Subscriber(f"/{self.name}/camera_node/image/compressed", CompressedImage, callback, queue_size=1)
 
-    def decode_image(self,image):
-        bridge = CvBridge()
+    def decode_image(self,image): 
+        """
+        Decode the compressed image. \n
+        Returns:
+            decoded_img: Image in format BGR if is decoded else returns nothing.
+        """ 
         try:
             self.img_count +=1
-            decoded_img = bridge.compressed_imgmsg_to_cv2(image)
+            decoded_img = self.bridge.compressed_imgmsg_to_cv2(image)
         except CvBridgeError as e:
             print(e)
             return None
         return decoded_img
 
     def save_image(self,location,image):
-        # moze da se napravi da zacivuva sekoi 5 sliki takva rab
+        """
+        Save image for debugging to a desired location. If the location doesn't exist it creates it.\n
+        Arguments:
+            location: Name of the folder.
+            image: The image to save to the folder.
+        """ 
         if not os.path.isdir(f"/data/{location}/"):
             os.mkdir(f"/data/{location}/")
         cv2.imwrite(f"/data/{location}/"+str(self.img_count)+".jpg",image)
         
     def publish_car_cmd(self,velocity,omega):
+        """
+        Publish target velocity and heading for the car.\n
+        Arguments:
+            velocity: Accepts range of (-1,1).
+            omega: Accepts  range of (-8,8).
+        """ 
         cmd_msg = Twist2DStamped()
         cmd_msg.v = velocity
         cmd_msg.omega = omega
@@ -69,14 +97,25 @@ class Duckiebot(DTROS):
         rospy.loginfo("Published speed for car (%s) and direction (%s)", str(cmd_msg.v), str(cmd_msg.omega))
 
     def publish_wheel_cmd(self,velocity_left,velocity_right):
+        """
+        Publish target velocity for each wheel.\n
+        Arguments:
+            velocity_left:  Accepts range of (-1,1).
+            velocity_right: Accepts  range of (-1,1).
+        """ 
         cmd_msg = WheelsCmdStamped()
         cmd_msg.vel_left = velocity_left
         cmd_msg.vel_right = velocity_right
         self.pub_wheel.publish(cmd_msg)
         rospy.loginfo("Published speed for left (%s) and right (%s) wheel", str(cmd_msg.vel_left), str(cmd_msg.vel_right))
     
-    def setupParam(self,param_name,default_value):
-        value = rospy.get_param(param_name,default_value)
+    def setupParam(self,param_name,value):
+        """
+        Set parameter for the robot.\n
+        Arguments:
+            param_name:  Name of the parameter.
+            value: Value to set.
+        """ 
         rospy.set_param(param_name,value)
         rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
         return value
